0e28e98f26814bbf6a3d5dfe5ca6d0f99089440c
EdilsonMboa <109667847+EdilsonMboa@users.noreply.github.com>
2025-11-04 12:04:57 +0200

    Learned how to pass params.

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
+from itertools import product
+
 from django.urls import path
 
-from .views import index, contacto
+from .views import index, contact, product
 
 urlpatterns = [
-    path('', index),
-    path('contacto', contacto),
+    path('', index, name='index'),
+    path('contact', contact, name='contact'),
+    path('product/<int:id>', product, name='product'),
 ]
