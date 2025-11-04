392dc952868708c622c56b22cf9e2ee9c742a7be
EdilsonMboa <109667847+EdilsonMboa@users.noreply.github.com>
2025-11-04 09:35:09 +0200

    Reinforced the knowledge of the importance of imports in django framework. Still, we really need work on each file so that we don't lose focus. Always keep the code clean.

--- core/admin.py
+++ core/admin.py
@@ -2,5 +2,13 @@ from django.contrib import admin
 
 from .models import Product, Client
 
-admin.site.register(Product)
-admin.site.register(Client)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'price', 'stock')
+    list_filter = ('name', 'price', 'stock')
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'email')
+    list_filter = ('name', 'surname', 'email')
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Client, ClientAdmin)
