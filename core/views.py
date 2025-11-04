697a792693c90d2cff185c57081623839bf54dfc
Edilson Mboa <ejmboa@gmail.com>
2025-11-04 12:15:04 +0200

    Learned how to pass params. (#5)
    Learned how to pass params.
    Database errors corrected (Dont know how)

--- core/views.py
+++ core/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render
+from core.models import Product
 
 def index(request):
-    return render(request, 'index.html')
+    products = Product.objects.all()
+    context = {
+        'products': products
+    }
+    return render(request, 'index.html', context)
 
-def contacto(request):
+def contact(request):
     return render(request, 'contacto.html')
+
+def product(request, id):
+    product = Product.objects.get(id=id)
+    context = {
+        'product' : product
+    }
+    return render(request, 'product.html', context)
